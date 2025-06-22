049f94d844bd1b0ed815c91b946cacc52e812b8f
boostmachine <184125960+boostmachine@users.noreply.github.com>
2025-06-23 02:47:17 +0300

    Feat: fixed js code

--- script.js
+++ script.js
@@ -53,3 +53,45 @@ function playGame(a) {
 }
 
 playGame(5)
+/* 
+function sumOfTripledEvens(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    // Step 1: If the element is an even number
+    if (array[i] % 2 === 0) {
+      // Step 2: Multiply this number by three
+      const tripleEvenNumber = array[i] * 3;
+
+      // Step 3: Add the new number to the total
+      sum += tripleEvenNumber;
+    }
+  }
+  return sum;
+}
+
+function sumOfTripledEvens2(array) {
+  return array 
+    .filter((num) => num % 2 === 0)
+    .map((num) => num * 3)
+    .reduce((acc, curr) => acc + curr);
+}
+
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+
+
+function camelize(string) {
+  return string.toLowerCase()
+    .split("-")
+    .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+    .join("");
+}
+
+
+function filterRange(array, a, b) {
+  return array
+    .filter(item => (a <= item && item <= b));
+}
+ */
